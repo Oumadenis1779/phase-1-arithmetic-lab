5f8bf695221a487bd6b19cac720d81d7d914dd14
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-09-22 19:13:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2 ;
+console.log ; multiply;
+
+const random = Math.floor(Math.random () * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+console.log (mod);
+
+const num5 = 15;
+const num6 = 18;
+const num7 = 20;
+var max = 20;
+console.log(max);
+
